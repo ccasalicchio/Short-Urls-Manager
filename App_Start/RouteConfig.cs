48b71c8a4137ca0c5a7cbb26d3aff45a840e5f26
Carlos Casalicchio <carlos.casalicchio@gmail.com>
2016-02-18 20:10:36 -0200

    changed manage link

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -24,6 +24,7 @@ namespace Shorten_Urls
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
             //routes.MapRoute(
             //    name: "Default",
             //    url: "{controller}/{action}/{id}",
